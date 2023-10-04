59764f2b4c307b02f26e48a35b4167fccec25fd4
21-manic-001g <21-manic-001g@21-manic-001>
2023-10-04 17:48:34 +0900

    1004

---
+++ JavaBook/src/java4/p11/Text.java
@@ -0,0 +1,16 @@
+package java4.p11;
+
+import java.util.Date;
+
+public class Text {
+
+	public static void main(String[] args) {
+		Object obj1 = new Object();
+		Date obj2 = new Date();
+		
+		System.out.println(obj1.toString());
+		System.out.println(obj2.toString());
+
+	}
+
+}
