474822a4dbb159536c607657b63fb566a62ee7a9
21-manic-001g <21-manic-001g@21-manic-001>
2023-10-19 17:46:28 +0900

    1019

---
+++ JavaBook/src/java5/p10/ThreadA.java
@@ -0,0 +1,12 @@
+package java5.p10;
+
+public class ThreadA extends Thread{
+	public ThreadA()	{
+		setName("ThreadA");
+	}
+	public void run() {
+		for(int i = 0; i < 2 ; i++) {
+			System.out.println(getName()+"가 출력한 내용");
+		}
+	}
+}
